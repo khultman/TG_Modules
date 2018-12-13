aae09848f865871c7ba60a55688b0084655c0730
Kyle Hultman <khultman@gmail.com>
2018-12-13 14:06:08 -0500

    hacking around route table ID selection

--- AWS_VPC_Peering/main.tf
+++ AWS_VPC_Peering/main.tf
@@ -83,12 +83,14 @@ resource "aws_vpc_peering_connection_accepter" "accepter" {
 }
 
 locals {
-  requester_pub_subnet_id = "${data.terraform_remote_state.requester_state.aws_route_table.public[var.requester_route_table_idx].id == "" ?
-                               data.terraform_remote_state.requester_state.aws_route_table.public.id :
-                               data.terraform_remote_state.requester_state.aws_route_table.public[var.requester_route_table_idx].id}"
-  requester_pri_subnet_id = "${data.terraform_remote_state.requester_state.aws_route_table.private[var.requester_route_table_idx].id == ""?
-                               data.terraform_remote_state.requester_state.aws_route_table.private.id :
-                               data.terraform_remote_state.requester_state.aws_route_table.private[var.requester_route_table_idx].id}"
+  requester_pub_subnet_id = "${data.terraform_remote_state.requester_state.aws_route_table.public[var.requester_route_table_idx].id != "" ?
+                               data.terraform_remote_state.requester_state.aws_route_table.public[var.requester_route_table_idx].id :
+                               data.terraform_remote_state.requester_state.aws_route_table.public.id}"
+
+  requester_pri_subnet_id = "${data.terraform_remote_state.requester_state.aws_route_table.private[var.requester_route_table_idx].id != ""?
+                               data.terraform_remote_state.requester_state.aws_route_table.private[var.requester_route_table_idx].id :
+                               data.terraform_remote_state.requester_state.aws_route_table.private.id}"
+
 }
 resource "aws_route" "requester_to_accepter_route" {
   //provider = "${data.terraform_remote_state.requester_state.provider ? data.terraform_remote_state.requester_state.provider : var.provider}"
@@ -98,12 +100,14 @@ resource "aws_route" "requester_to_accepter_route" {
 }
 
 locals {
-  accepter_pub_subnet_id = "${data.terraform_remote_state.accepter_state.aws_route_table.public[var.accepter_route_table_idx].id == "" ?
-                              data.terraform_remote_state.accepter_state.aws_route_table.public.id :
-                              data.terraform_remote_state.accepter_state.aws_route_table.public[var.accepter_route_table_idx].id}"
-  accepter_pri_subnet_id = "${data.terraform_remote_state.accepter_state.aws_route_table.private[var.accepter_route_table_idx].id == "" ?
-                              data.terraform_remote_state.accepter_state.aws_route_table.private.id :
-                              data.terraform_remote_state.accepter_state.aws_route_table.private[var.accepter_route_table_idx].id}"
+  accepter_pub_subnet_id = "${data.terraform_remote_state.accepter_state.aws_route_table.public[var.accepter_route_table_idx].id != "" ?
+                              data.terraform_remote_state.accepter_state.aws_route_table.public[var.accepter_route_table_idx].id :
+                              data.terraform_remote_state.accepter_state.aws_route_table.public.id}"
+
+  accepter_pri_subnet_id = "${data.terraform_remote_state.accepter_state.aws_route_table.private[var.accepter_route_table_idx].id != "" ?
+                              data.terraform_remote_state.accepter_state.aws_route_table.private[var.accepter_route_table_idx].id :
+                              data.terraform_remote_state.accepter_state.aws_route_table.private.id}"
+
 }
 resource "aws_route" "appter_to_requester_route" {
   //provider = "${data.terraform_remote_state.accepter_state.provider ? data.terraform_remote_state.accepter_state.provider : var.provider}"
