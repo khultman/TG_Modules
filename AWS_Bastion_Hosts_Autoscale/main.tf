277b83538b2fc4eef795c9d1cf48ce0445be5f80
Kyle Hultman <khultman@gmail.com>
2018-12-18 15:41:05 -0500

    changed lb formatting

--- AWS_Bastion_Hosts_Autoscale/main.tf
+++ AWS_Bastion_Hosts_Autoscale/main.tf
@@ -58,7 +58,7 @@ resource "aws_security_group" "bastion_sg" {
 resource "aws_security_group_rule" "bastion_sg_rule_ssh" {
   description = "Allow worker nodes to communicate with each other"
   from_port = "${var.bastion_host_ssh_port}"
-  protocol = "tcp"
+  protocol = "TCP"
   security_group_id = "${aws_security_group.bastion_sg.id}"
   cidr_blocks  = ["0.0.0.0/0"]
   to_port = "${var.bastion_host_ssh_port}"
